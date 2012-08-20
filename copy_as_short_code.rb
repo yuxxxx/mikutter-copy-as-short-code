ce43a5227489f843eb3853f319c1f4b1cea2e80f
yuxxxx <marlcabbage@gmail.com>
2012-08-20 12:06:53 +0900

    mikutter0.2に対応させました

--- copy_as_short_code.rb
+++ copy_as_short_code.rb
@@ -5,23 +5,15 @@
 
 Plugin.create(:copy_as_short_code) do
 
-  filter_command do |menu|
-    menu[:copy_as_short_code] = {
-      :slug => :copy_as_short_code,
-      :name => 'ショートコード形式でコピー',
-      :condition => lambda{ |opt|
-        true
-      },
-      :exec => lambda{ |opt|
-        message = opt.message
-        screen_name = message.user[:idname]
-        Gtk::Clipboard.copy("[tweet http://twitter.com/#!/#{screen_name}/status/#{message.id} align='#{UserConfig[:copy_as_short_code_align]}' lang='ja']")
-      },
-      :visible => true,
-      :role => :message }
-    [menu]
-  end
-
+ command(:copy_as_short_code,
+         name: 'ショートコード形式でコピー',
+         condition: lambda{ |opt| true },
+         visible: true,
+         role: :timeline) do |opt|
+    message = opt.message
+    screen_name = message.user[:idname]
+    Gtk::Clipboard.copy("[tweet http://twitter.com/#!/#{screen_name}/status/#{message.id} align='#{UserConfig[:copy_as_short_code_align]}' lang='ja']") end
+  
   settings("ショートコードでコピペ") do
     select('配置', :copy_as_short_code_align) do
       option "left", "左" 
