3b0419fa30cd1905829a433c753ab7d3e64afe4f
yuxxxx <marlcabbage@gmail.com>
2012-09-11 07:21:49 +0900

    複数ツイートの同時コピーに対応 	modified:   copy_as_short_code.rb

--- copy_as_short_code.rb
+++ copy_as_short_code.rb
@@ -9,25 +9,18 @@ Plugin.create(:copy_as_short_code) do
     menu[:copy_as_short_code] = {
       :slug => :copy_as_short_code,
       :name => 'ショートコード形式でコピー',
-      :condition => lambda{ |opt|
-        true
-      },
-      :exec => lambda{ |opt|
-        message = opt.message
-        screen_name = message.user[:idname]
-        Gtk::Clipboard.copy("[tweet http://twitter.com/#!/#{screen_name}/status/#{message.id} align='#{UserConfig[:copy_as_short_code_align]}' lang='ja']")
+      :condition => lambda{ |opt| true },
+      :exec => lambda{ |messages|
+        short_code = messages.map{|opt|
+          message = opt.message
+          screen_name = message.user[:idname]
+          "[tweet http://twitter.com/#!/#{screen_name}/status/#{message.id} lang='ja']"
+        }.join ' '
+        Gtk::Clipboard.copy(short_code)
       },
       :visible => true,
-      :role => :message }
+      :role => :messages }
     [menu]
-  end
-
-  settings("ショートコードでコピペ") do
-    select('配置', :copy_as_short_code_align) do
-      option "left", "左" 
-      option "right", "右" 
-      option "center", "中央" 
-    end
   end
 end
 
